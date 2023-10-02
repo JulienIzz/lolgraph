e55019b78f6b201854d5d2f16bc55a8cc6c1b449
JulienIzz <julien.izzillo@gmail.com>
2023-10-02 19:14:33 +0200

    feat: adding user need inputs in home and passing them to next page

--- src/modules/home/Home.screen.test.tsx
+++ src/modules/home/Home.screen.test.tsx
@@ -1,16 +1,26 @@
 import { render, screen } from "@testing-library/react-native";
 import { Home } from "./Home.screen";
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+jest.mock("@react-navigation/native", () => ({
+  ...jest.requireActual("@react-navigation/native"),
+  useNavigation: () => ({ navigate: jest.fn() }),
+}));
 
 test("home screen with title is correctly rendered", async () => {
-  // PARTIE 1 : On rend le composant avec les constantes qu'on veut
   const expectedHeaderTextForTest = "MyLEAGUE";
 
-  render(<Home />);
+  render(
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Home />
+      </NavigationContainer>
+    </SafeAreaProvider>,
+  );
 
-  // PARTIE 2 : On cherche les données qu'on veut vérifier dans le composant rendu / On vérifie les comportements
-  const HeaderOutput = screen.getByText(expectedHeaderTextForTest);
+  const HeaderOutput = screen.findByText(expectedHeaderTextForTest);
 
-  // PARTIE 3 : On fait des vérifications
   expect(HeaderOutput).toBeTruthy();
   expect(screen.toJSON()).toMatchSnapshot();
 });
--- src/modules/home/Home.screen.tsx
+++ src/modules/home/Home.screen.tsx
@@ -1,12 +1,102 @@
-import { View } from "react-native";
 import { BaseScreenNoBack } from "../../shared/screen/BaseScreenNoBack";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import styled from "@emotion/native";
+import { Spacer } from "../../shared/screen/Spacer";
+import { Dropdown } from "react-native-element-dropdown";
+import { StyleSheet } from "react-native";
+import { theme } from "../../shared/theme/theme";
 
 export type HomeRouteProps = undefined;
 
 export const Home = () => {
+  const navigation = useNavigation();
+  const [input, setInput] = useState<string | null>();
+  const [selectedServer, setSelectedServer] = useState<string | null>();
+
   return (
     <BaseScreenNoBack title="MyLEAGUE">
-      <View />
+      <Container>
+        <Spacer vertical={75} />
+        <ProfileNameInput
+          onChangeText={(input) => setInput(input)}
+          placeholder="Summoner's name"
+          placeholderTextColor={"gray"}
+        />
+        <Spacer vertical={12} />
+        <Dropdown
+          data={servers}
+          labelField={"label"}
+          valueField={"value"}
+          onChange={(item) => setSelectedServer(item.value)}
+          placeholder="Server"
+          placeholderStyle={styles.dropdownPlaceholder}
+          selectedTextStyle={styles.dropdownLabels}
+          style={styles.dropdown}
+        />
+        <Spacer vertical={40} />
+        <SearchButton
+          onPress={() => {
+            selectedServer && input
+              ? navigation.navigate("Profile", {
+                  userId: input,
+                  server: selectedServer,
+                })
+              : null;
+          }}
+        >
+          <SearchButtonText>Search</SearchButtonText>
+        </SearchButton>
+      </Container>
     </BaseScreenNoBack>
   );
 };
+
+const ProfileNameInput = styled.TextInput({
+  width: 250,
+  height: 40,
+  borderColor: "gray",
+  borderRadius: 6,
+  borderWidth: 1,
+  textAlign: "center",
+  fontSize: 16,
+});
+
+const SearchButton = styled.TouchableOpacity({
+  height: 40,
+  width: 250,
+  backgroundColor: theme.primary,
+  borderColor: "gray",
+  borderRadius: 6,
+  borderWidth: 1,
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+const SearchButtonText = styled.Text({
+  fontSize: 16,
+  fontWeight: "bold",
+});
+
+const Container = styled.View({
+  flex: 1,
+  margin: 25,
+  alignItems: "center",
+});
+
+const servers = [
+  { label: "EUW", value: "EUW1" },
+  { label: "NA", value: "NA1" },
+];
+
+const styles = StyleSheet.create({
+  dropdown: {
+    width: 250,
+    height: 40,
+    borderColor: "gray",
+    borderRadius: 6,
+    borderWidth: 1,
+  },
+  dropdownPlaceholder: { color: "gray", textAlign: "center" },
+  dropdownLabels: { textAlign: "center" },
+});
